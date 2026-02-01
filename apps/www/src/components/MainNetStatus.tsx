0059db1e2a6fefe8a62adde8fd3c45412f8d3e1c
Winner960111 <chenyih0111@gmail.com>
2026-02-01 08:27:07 -0500

    Update(main):header updating

--- apps/www/src/components/MainNetStatus.tsx
+++ apps/www/src/components/MainNetStatus.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export const MainNetStatus = () => {
   return (
-    <div className="w-full my-8 flex justify-center items-center gap-2.5 font-medium font-sans">
+    <div className="w-full pt-16 pb-8 flex justify-center items-center gap-2.5 font-medium font-sans">
       <div className="rounded-[4px] py-0.5 px-1.5 text-sm text-[#0075FF]">Mainnet Beta</div>
       <h1 className="text-[16px] text-white">v2.4.0 is Live</h1>
       <button className='flex items-center gap-1.5 transition-all text-sm bg-white text-black hover:scale-105 rounded-full py-1 px-1.5'>
