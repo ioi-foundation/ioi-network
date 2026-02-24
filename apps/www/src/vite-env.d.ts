50301b83c8b72867484cf82b56bbe333ed26b787
papabear <papabear@decentralized.cloud>
2026-02-24 12:05:01 -0500

    chore(www): remove unused Lottie codepaths

--- apps/www/src/vite-env.d.ts
+++ apps/www/src/vite-env.d.ts
@@ -4,13 +4,3 @@ declare module "*.svg" {
   const src: string;
   export default src;
 }
-
-declare module "*.lottie?url" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.json" {
-  const value: unknown;
-  export default value;
-}
